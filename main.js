98b253894cbae2ffc153c868bad5d8572a7e3add
abimael <abibarre.82@gmail.com>
2019-10-24 23:10:05 -0400

    Version 1

---
+++ main.js
@@ -0,0 +1,20 @@
+
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".hire-btn");
+var closeButton = document.querySelector(".close");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+
