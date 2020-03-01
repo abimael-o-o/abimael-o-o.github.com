e102f59f1bb3018db905daf55e3b55977cea7257
abimael <abibarre.82@gmail.com>
2020-03-01 00:47:14 -0500

    Updates responsive nav

--- main.js
+++ main.js
@@ -3,16 +3,25 @@ const btn = document.getElementById('btn-toggle');
 const navbar = document.getElementById('navbar');
 const btnX = document.getElementById('btnX');
 
-btn.onclick  = () => {
-    nav.style.display = 'block';
-    navbar.style = 'height: 100%'
-}
-btnX.onclick = () => {
-    nav.style.display = 'none';
-    navbar.style = 'height: 100px'
-}
-window.onclick = (e) => {
-    if(e.target == nav){
-        nav.style.display = 'none';
+
+if(window.innerWidth <= 600 ){
+    console.log('yes');
+    btn.onclick  = () => {
+        nav.style.display = 'block';
+        navbar.style = 'height: 100%'
     }
+    btnX.onclick = () => {
+        nav.style.display = 'none';
+        navbar.style = 'height: 100px'
+    }
+    window.onclick = (e) => {
+        if(e.target == nav){
+            nav.style.display = 'none';
+        }
+    }
+        nav.style.display = 'none';
+        navbar.style = 'height: 100px' 
+
+
+    
 }
