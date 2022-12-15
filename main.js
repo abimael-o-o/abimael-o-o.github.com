e9f6414b736868d30130b126245b22d457188ee8
Me <me@mes-macbook-pro.attlocal.net>
2022-12-14 19:29:45 -0500

    Change intro and remove unused files.

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-const nav = document.getElementById('nav');
-const btn = document.getElementById('btn-toggle');
-const navbar = document.getElementById('navbar');
-const btnX = document.getElementById('btnX');
-
-
-if(window.innerWidth <= 600 ){
-    btn.onclick  = () => {
-        nav.style.display = 'block';
-        navbar.style = 'height: 100%'
-    }
-    btnX.onclick = () => {
-        nav.style.display = 'none';
-        navbar.style = 'height: 100px'
-    }
-    window.onclick = (e) => {
-        if(e.target == nav){
-            nav.style.display = 'none';
-        }
-    }
-};
-
 let cursor = true;
 let speed = 500;
 
